2c3dd943756848f82d7262ede3f515d84d44ea28
Joaquin <joaquinmartinezfranco@gmail.com>
2023-04-29 13:01:47 -0300

    DATOS

---
+++ src/negocio/SubSistemaDatos.java
@@ -0,0 +1,16 @@
+package negocio;
+
+public class SubSistemaDatos {
+	AbonadosSinContratacion abonadosSinContratacion;
+	DatosFacturas datosFacturas;
+	
+	
+	public SubSistemaDatos() {
+		datosFacturas=new DatosFacturas();
+		abonadosSinContratacion=new AbonadosSinContratacion();
+	}
+	
+	public existeAbonado();
+	
+	publi
+}
