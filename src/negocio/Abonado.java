38aa8a4ba538875c9adcf4d2834f881df9f440a4
ramirocardelli <ramirocardelli89@gmail.com>
2023-04-29 12:15:53 -0300

    <br>

--- src/negocio/Abonado.java
+++ src/negocio/Abonado.java
@@ -1,7 +1,7 @@
 package negocio;
 
-/** Esta clase representa un abonado dentro de un sistema de contratación de un servicios de seguridad.
- * Contiene información sobre su nombre y su dni.
+/** Esta clase representa un abonado dentro de un sistema de contrataciï¿½n de un servicios de seguridad.
+ * Contiene informaciï¿½n sobre su nombre y su dni.
  */
 public abstract class Abonado implements Cloneable{  
     private String nombre;
@@ -10,7 +10,7 @@ public abstract class Abonado implements Cloneable{
 
     /** Constructor de 2 parametros String para crear un nuevo abonado.
      * @param nombre : Nombre del abonado.
-     * @param dni : Numero de documento de identidad del abonado.
+     * @param dni : Numero de documento de identidad del abonado. <br>
      * <b> Pre: </b> nombre y dni no pueden ser nulos ni " ". <br>
      * <b> Post: </b> Se crea un nuevo abonado con nombre y dni.
      */
@@ -38,7 +38,7 @@ public abstract class Abonado implements Cloneable{
 
     /** Metodo para clonar un abonado.
      * @return : Se devuelve un clon del abonado correspondiente.
-     * @throws CloneNotSupportedException : Se lanza una excepcion cuando el abonado es de tipo persona jurídica, la cual no puede aceptar clonacion.
+     * @throws CloneNotSupportedException : Se lanza una excepcion cuando el abonado es de tipo persona jurï¿½dica, la cual no puede aceptar clonacion.
      */
     public Object clon() throws CloneNotSupportedException{
     	Abonado clon= (Abonado)super.clone();
