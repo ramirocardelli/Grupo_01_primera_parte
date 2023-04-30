8dc970f6e941ac571a2266a7bf808c8bb5fbe638
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2023-04-30 18:48:13 -0300

    JavadocMain
    :O

--- src/presentacion/Prueba.java
+++ src/presentacion/Prueba.java
@@ -104,7 +104,15 @@ import negocio.*;
 			System.out.println("MUESTRA CLON: \n"+clon3);
 			sistema.MuestraEstado();
 		}
+	
 		
+	/** Metodo que envia a sistema el mensaje de cargar un abonado.
+	* @param nombre : el nombre del abonado que se ingresa.<br>
+	* @param dni : el dni del abonado que se ingresa.<br>
+	* @param tipo: tipo de abonado ingresante.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Se agrega el abonado a la lista de abonados sin contratacion o sino se imprime por pantalla el problema surgido<br>
+	*/	
 	private static void ingresaAbonado(String nombre,String dni, String tipo) {
 		if(nombre!=null && !nombre.equals("") && dni!=null && !dni.equals("") && !tipo.equals("")) { //tipo si puede ser null, ya que implica que es la factura default
 			try {
@@ -126,6 +134,17 @@ import negocio.*;
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 	}
 	
+	
+	/** Metodo que envia a sistema el mensaje de cargar una contratacion a una factura existente.
+	* @param dni : el dni del abonado a cuya factura quiere agregarse la contratacion.<br>
+	* @param camaras: cantidad de camaras contratadas en el servicio.<br>
+	* @param botonesAntipanico: cantidad de botones antipanico contratados en el servicio.<br>
+	* @param movilAcompanamiento: booleano que responde a si se contrata un movil de acompanamiento.<br>
+	* @param domicilio: direccion de la propiedad sobre la que se contrata el servicio.<br>
+	* @param tipo: el tipo de la contratacion que se ingresa.<br>
+	* <b> Pre: </b> No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales)<br>
+	* <b> Post: </b> Se agrega la contratacion a la lista de contrataciones de la factura correspondiente al dni ingresado o sino se imprime por pantalla el problema surgido<br>
+	*/	
 	private static void ingresaContratacion(String dni,int camaras, int botonesAntipanico, boolean movilAcompanamiento, Domicilio domicilio, String tipo) {
 		if(dni!=null && !dni.equals("") && camaras>=0 && botonesAntipanico>=0 && domicilio!=null && tipo!=null && !tipo.equals("")) {
 			try {
@@ -144,7 +163,13 @@ import negocio.*;
 		else
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");	
 	}
-	
+	/** Metodo que envia a sistema el mensaje de cargar una factura en el sistema.
+	* @param dni: el dni de un abonado sin contratacion al que se le creara una factura.<br>
+	* @param tipoPago: metodo de pago seleccionado para la factura nueva.<br>
+	* @param tipoFactura: el tipo de la nueva factura creada.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Se agrega la contratacion a la lista de contrataciones de la factura correspondiente al dni ingresado o sino se imprime por pantalla el problema surgido<br>
+	*/	
 	private static void ingresaFactura(String dni, String tipoPago, String tipoFactura) {
 		if(dni!=null && !dni.equals("")&& tipoPago!=null && !tipoPago.equals("")) { //el tipo de factura puede ser null, en tal caso la factura se crea de tipo default
 			try {
@@ -167,6 +192,11 @@ import negocio.*;
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");	
 	}
 	
+	/** Metodo que envia a sistema el mensaje de eliminar una factura de su base de datos.
+	* @param dni: el dni de la factura que se quiere eliminar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b>Se elimina la factura seleccionada de la lista de facturas en base al dni recibido por parametro o sino se imprime por pantalla el problema surgido  <br>
+	*/
 	private static void eliminaFactura(String dni) {
 		if(dni!=null && !dni.equals("")) {
 			try {
@@ -180,7 +210,11 @@ import negocio.*;
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 		}
 	}
-	
+	/**Metodo que envia a sistema el mensaje de eliminar un abonado sin contratacion de su base de datos.
+	* @param dni: el dni del abonado que se quiere eliminar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b>Se elimina el abonado seleccionado de la lista de abonados sin contratacion en base al dni recibido por parametro o sino se imprime por pantalla el problema surgido  <br>
+	*/
 	private static void eliminaAbonadoSinContratacion(String dni) {
 		if(dni!=null && !dni.equals("")) {
 			try {
@@ -197,7 +231,12 @@ import negocio.*;
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 		}
 	}
-	
+	/**Metodo que envia a sistema el mensaje de eliminar una contratacion de un abonado.
+	* @param dni: el dni del abonado del que se quiere eliminar la contratacion.<br>
+	* @param domicilio: el domicilio que posee la contratacion que se desea eliminar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b>Se elimina la contratacion (seleccionada por su domicilio) del abonado especificado por dni o sino se imprime por pantalla el problema surgido  <br>
+	*/
 	private static void eliminaContratacionAbonado(String dni,Domicilio domicilio) {
 		if(dni!=null && !dni.equals("") && domicilio!=null) {
 			try {
@@ -215,6 +254,13 @@ import negocio.*;
 		}
 	}
 	
+	
+	/**Metodo que envia a sistema el mensaje de aplicar una promocion a un domicilio.
+	* @param domicilio: domicilio sobre el cual se quiere aplicar la promocion.<br>
+	* @param promocion: promocion (dorada o plateada) que se aplicara sobre el domicilio.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Se aplica el descuento sobre el precio del servicio del domicilio, dado por el tipo de promocion especificada o sino se imprime por pantalla el problema surgido  <br>
+	*/
 	private static void aplicaPromocion(Domicilio domicilio, Promo promocion) {
 		if(domicilio!=null && promocion!=null) {
 			try {
@@ -227,7 +273,12 @@ import negocio.*;
 		else
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 	}
-	
+	/**Metodo que pide a sistema el precio de una factura y lo muestra por pantalla.
+	* @param dni:el dni del abonado asociado a esa factura.<br>
+	* @param descuento: booleano que indica si el precio se quiere con o sin el descuento por metodo de pago.<br>
+	* <b> Pre: </b> No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Imprime por pantalla el precio (con o sin descuento) de la factura asociada al dni o sino se imprime por pantalla el problema surgido<br>
+	*/
 	private static void FacturaAPagar(String dni,boolean descuento) {
 		if(dni!=null && !dni.equals("")){
 			try {
@@ -242,6 +293,11 @@ import negocio.*;
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 	}
 	
+	/**Metodo que solicita un clon de una factura para luego retornarlo.
+	* @param dni: el dni del abonado asociado a esa factura a clonar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Imprime por pantalla el precio (con o sin descuento) de la factura asociada al dni  o sino muestra por pantalla el problema surgido<br>
+	*/
 	private static IFactura clonaFactura(String dni) {
 		IFactura clon=null;
 		if(dni!=null && !dni.equals("")){
@@ -259,7 +315,11 @@ import negocio.*;
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 		return clon;
 	}
-	
+	/**Metodo solicita al sistema una factura, para luego mostrarla por pantalla.
+	* @param dni: el dni del abonado asociado a la factura a clonar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Imprime por pantalla la factura asociada al dni especificado o sino muestra por pantalla el problema surgido<br>
+	*/
 	private static void MuestraFactura(String dni) {
 		if(dni!=null && !dni.equals("")){
 				try {
@@ -272,6 +332,11 @@ import negocio.*;
 		else
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 	}
+	/**Metodo solicita al sistema un abonado sin factura, para luego mostrarlo por pantalla.
+	* @param dni: el dni del abonado que se quiere mostrar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Imprime por pantalla el abonado sin factura especificado por su dni o sino muestra por pantalla el problema surgido<br>
+	*/
 	private static void MuestraAbonadoSinFactura(String dni) {
 		if(dni!=null && !dni.equals("")){
 				try {
@@ -284,7 +349,11 @@ import negocio.*;
 		else
 			System.out.println("Parametros incorrectos, por favor reingresar informacion correctamente");
 	}
-	
+	/**Metodo solicita al sistema una contratacion, para luego mostrarla por pantalla.
+	* @param domicilio: domicilio sobre el cual se tiene la contratacion a mostrar.<br>
+	* <b> Pre: </b>  No cuenta con precondiciones(esto xq se controlan con excepciones o condicionales) <br>
+	* <b> Post: </b> Imprime por pantalla la contratacion especificada por su domicilio o sino muestra por pantalla el problema surgido<br>
+	*/
 	private static void MuestraContratacion(Domicilio domicilio) {
 		if(domicilio!=null){
 				try {
