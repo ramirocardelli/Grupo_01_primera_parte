b00b41956b3cba8e8c32af9ec4d584cf26901ea6
josefinafrasca <josefrasca16@gmail.com>
2023-06-11 10:01:28 -0300

    Revert "tecnicos concurrencia"
    This reverts commit 241d313ba7ff1f39a1681fa87361647290851155.

--- src/negocio/Tecnico.java
+++ src/negocio/Tecnico.java
@@ -1,40 +1,28 @@
 package negocio;
 
 import java.io.Serializable;
-import java.util.concurrent.*;
+import java.util.concurrent.TimeUnit;
 
 public class Tecnico implements Serializable{
-	protected String nombre;
-	protected Semaphore semaforo=new Semaphore(1,true);
-
-	public Tecnico(String nombre) {
-		super();
-		this.nombre = nombre;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		Tecnico tecnico;
-		if (obj instanceof Tecnico) {
-			tecnico=(Tecnico) obj;
-			return this.nombre.equalsIgnoreCase(tecnico.nombre);
-		}
-		return false;
-	}
+	public Abonado atendiendo;
 	
-	public boolean atender() {
-		if (semaforo.tryAcquire()==true) {
+	
+	public synchronized void solicitarTecnico() {
+		while (atendiendo!=null) {
+			System.out.println("Tecnico ocupado");
 			try {
-				TimeUnit.SECONDS.sleep(200);
+				wait();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			return true;
 		}
-		else
-			return false;
+		try {
+			TimeUnit.SECONDS.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		notifyAll();
 	}
-	
-	
 }
