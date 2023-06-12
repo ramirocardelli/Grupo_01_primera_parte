2344e432f5bf7d4548f62b61313af130493cbf52
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2023-06-11 21:33:54 -0300

    Act

--- src/negocio/SubSistemaDatos.java
+++ src/negocio/SubSistemaDatos.java
@@ -1,11 +1,9 @@
 package negocio;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Iterator;
-
-import modelo.AbonadosSinContratacion;
-import modelo.DatosFacturas;
-import modelo.Tecnicos;
 
 /**
  * Clase que funciona como interfaz entre Sistema y los Datos
@@ -16,50 +14,44 @@ import modelo.Tecnicos;
  *
  *
  */
-public class SubSistemaDatos implements Serializable {
-	private AbonadosSinContratacion abonadosSinContratacion;
-	private DatosFacturas datosFacturas;
-	private Tecnicos tecnicos;
+public class SubSistemaDatos {
+	private HashMap<String, Abonado>abonados=new HashMap<String, Abonado>();
 	
-	/**
-	 * Constructor de la clase instancia los ArrayList de la clase
-	 */
+	
 	public SubSistemaDatos() {
-		datosFacturas=new DatosFacturas();
-		abonadosSinContratacion=new AbonadosSinContratacion();
-		tecnicos=new Tecnicos();
+	}	
+	
+	public void nuevaContratacion(String dni, Contratacion nuevaContratacion) throws DomicilioYaConContratacionExcepcion, DniDesconocidoException {
+		Abonado buscaAbonado=this.abonados.get(dni);
+    	if(buscaAbonado!=null){
+    		Contratacion buscaContratacion=this.buscaContratacion(nuevaContratacion.domicilio);
+    		if(buscaContratacion==null) { 
+    			buscaAbonado.addContratacion(buscaContratacion);
+    		}
+    		else 
+    			throw new DomicilioYaConContratacionExcepcion(nuevaContratacion,buscaContratacion);
+    	}
+    	else
+    		throw new DniDesconocidoException(dni);
 	}
 	
-	
-	/**
-	 * Metodo que busca un Abonado asociado a un DNI que NO tenga una contratacion asociada a el
-	 * @param dni DNI del Abonado buscado
-	 * @return Abonado asociado al DNI 
-	 */
-	public Abonado buscaAbonado(String dni) {
-		Iterator<Abonado>it=abonadosSinContratacion.getAbonados();
-		Abonado busqueda=null;
-		boolean encuentra=false;
-		while(it.hasNext() && !encuentra){
-			busqueda=it.next();
-			if(dni.equals(busqueda.getDni())) {
-				encuentra=true;
-			}
+	public void eliminarContratacion(String dni,Domicilio domicilio) throws DomicilioSinContratacionEnAbonadoException, DniDesconocidoException {
+		Abonado abonado= abonados.get(dni);
+		if(abonado!=null) {
+			abonado.eliminaContratacion(domicilio);
 		}
-		if(!encuentra)
-			busqueda=null;
-		return busqueda;
+		else {
+			throw new DniDesconocidoException(dni);
+		}
 	}
 	
 	
-	/**
-	 * Metodo que busca una factura asociada a un Domicilio y la devuelve
-	 * @param domicilio : Domicilio del que se desea obtener la contratacion asociada
-	 * @return Contratacion con la contratacion pedida
-	 * <b>Post</b> La contratacion si es encontrada se devuelve la referencia a esa Contratacion , si no se encuentra la contratacion se devuelve null
-	 */
-	public Contratacion buscaContratacion(Domicilio domicilio) { //Ingresando un domicilio busca la contratacion
-		Iterator<IFactura>it=this.datosFacturas.getFacturas();
+	public Abonado buscaAbonado(String dni)  {
+		return abonados.get(dni);
+	}
+	
+	public Contratacion buscaContratacion(Domicilio domicilio)  { //Ingresando un domicilio busca la contratacion
+		Iterator<Abonado>it=abonados.values().iterator();
 		Contratacion rta = null;
 		boolean encuentra=false;
 		while(it.hasNext() && !encuentra){
@@ -71,95 +63,80 @@ public class SubSistemaDatos implements Serializable {
 			rta=null;
 		return rta;
 	}
-	/**
-	 * Metodo que Busca una factura asociada al DNI de un Abonado
-	 * @param dni : DNI del Abonado
-	 * @return IFactura con la factura pedida
-	 *  <b>Post</b> Se devuelve una Factura si se encuentra un abonado asociado a ella o null si no se encuentra dicha Factura
-	 */
-	public IFactura buscaFactura(String dni) {
-		Iterator<IFactura>it=this.datosFacturas.getFacturas();
-		IFactura rta = null;
-		boolean encuentra=false;
-		while(it.hasNext() && !encuentra){
-			rta=it.next();
-			String rtadni=rta.getAbonado().getDni();
-			if(rtadni.compareTo(dni)==0) {
-				encuentra=true;
-			}
-		}
-		if(!encuentra)
-			rta=null;
-		return rta;
-	}
-	/**
-	 * Metodo que agrega una factura a la lista de facturas
-	 * @param factura : Factura que se desea añadir a la lista
-	 * <b>Pre</b> factura debe ser diferente de null.
-	 */
-	public void agregaFactura(IFactura factura) {
-		this.datosFacturas.agregaFactura(factura);
-	}
-	/**
-	 * Elimina de la lista de Abonados sin contratacion al abonado dado.
-	 * @param abonado : Instancia de Abonado
-	 * <b>Pre</b> Abonado debe ser diferente de null.
-	 */
-	public void agregaAbonadoSinFactura(Abonado abonado) {
-		this.abonadosSinContratacion.agregaAbonado(abonado);
-	}
-	/**
-	 * Metodo que elimina una Factura asociado a un Abonado mediante un DNI provisto.
-	 * @param dni : DNI del abonado
-	 * @throws DniDesconocidoException Se lanza esta excepcion si no hay un Abonado asociado a este DNI
-	 */
-	public void eliminaFactura(String dni) throws DniDesconocidoException {
-		IFactura elimina=buscaFactura(dni);
-		if(elimina!=null) {
-			agregaAbonadoSinFactura(elimina.getAbonado());
-			this.datosFacturas.eliminaFacturas(elimina);
-			
+	
+	public IFactura buscaFactura(String dni,GregorianCalendar mesYanio) throws DniDesconocidoException { //Ingresando un domicilio busca la contratacion
+		Abonado buscaAbonado= abonados.get(dni);
+		IFactura rta=null;
+		if(buscaAbonado!=null) {
+			rta=buscaAbonado.getFactura(mesYanio);	
 		}
 		else {
 			throw new DniDesconocidoException(dni);
 		}
-		
+		return rta;
 	}
-	/**
-	 * Metodo que elimina un Abonado de la lista de AbonadosSinContratacion 
-	 * @param dni : DNI del Abonado
-	 * @throws DniDesconocidoException Excepcion lanzada si no se pudo encontrar un Abonado que este asociado a el DNI provisto
-	 * @throws AbonadoYaCargadoException Excepcion lanzada si el Abonado que se desea eliminar ya fue cargado en la lista de Facturas
-	 */
-	public void eliminaAbonadoSinFactura(String dni) throws DniDesconocidoException, AbonadoYaCargadoException{ 
-		Abonado elimina=buscaAbonado(dni);
-		if(elimina!=null)
-			this.abonadosSinContratacion.eliminaAbonado(elimina);
-		else {
-			Iterator<IFactura>it=this.datosFacturas.getFacturas();
-			IFactura busca=null;
-			boolean encuentra=false;
-			while(it.hasNext() && !encuentra) {
-				busca=it.next();
-				if(busca.getAbonado().getDni().equals(dni))
-					encuentra=true;
+	
+	public void agregaAbonado(Abonado abonado) throws AbonadoYaCargadoException {
+		if(!this.abonados.containsKey(abonado.getDni())){ //verifica que abonado no este cargado en la lista de abonados sin contratacion	
+			abonados.put(abonado.getDni(), abonado);
+		}
+		else
+			throw new AbonadoYaCargadoException(abonado.getNombre(), abonado.getDni());
+	}
+	
+	
+	public void eliminaAbonado(String dni) throws DniDesconocidoException, AbonadoConFacturaException {
+		Abonado eliminar=this.abonados.get(dni);
+		if(eliminar!=null) {
+			if(eliminar.getFactura(null)==null) {
+				this.abonados.remove(dni);
 			}
-			if(!encuentra)
-				throw new DniDesconocidoException(dni);
 			else
-				throw new AbonadoYaCargadoException(dni, busca.getAbonado().getNombre(), true);
+				throw new AbonadoConFacturaException(dni, dni);
+		}
+			throw new DniDesconocidoException(dni);
+	}
+	
+	public Iterator<Abonado> iteratorAbonados(){
+		return this.abonados.values().iterator();
+	}
+
+	
+	public void findeMes(GregorianCalendar fecha) {
+		Iterator<Abonado> itAbonados= this.abonados.values().iterator();
+		ArrayList<Contratacion> listaContrataciones=new ArrayList<Contratacion>();
+		while(itAbonados.hasNext()) { //clona las contrataciones para que estan permanezcan constantes en el historico (no son referencias)
+			Abonado abonado=itAbonados.next();
+			listaContrataciones.clear();
+			Iterator<Contratacion> itContrataciones= abonado.getContrataciones();
+			while(itContrataciones.hasNext()) {
+				listaContrataciones.add(itContrataciones.next());
+			}
+			abonado.findeMes(new Factura(fecha, listaContrataciones));
 		}
 	}
 	
-	
-	
-	/**
-	 * Metodo que es un simple pasamanos. Pide un iterator a la capa de Datos y lo reenvia por el return
-	 * @return Iterator de todas las Facturas
-	 */
-	public Iterator<IFactura> iteratorFacturas() {
-		return datosFacturas.getFacturas();
+	public void pagaFactura(String dni,String metodoPago) throws DniDesconocidoException, MetodoDePagoInvalidoException,noHayFacturaAPagarException {
+		Abonado abonado=this.abonados.get(dni);
+		if(abonado!=null) {
+			IFactura aPagar=abonado.getFactura(null);
+			if(aPagar!=null) {
+				FactoryDecoradoFactura Fa= new FactoryDecoradoFactura();
+				IFactura pagada=Fa.creaFactura(aPagar, metodoPago); //obtiene la factura a pagar
+				abonado.pagaFactura(pagada);
+			}
+			else
+				throw new noHayFacturaAPagarException(dni);
+		}
+		else
+			throw new DniDesconocidoException(dni);
 	}
 	
-	
+	public void muestraEstado() {
+		Iterator<Abonado> it=this.abonados.values().iterator();
+		System.out.println("El sistema cuenta con las siguientes facturas:\n");
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
 }
